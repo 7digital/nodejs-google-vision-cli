415ed73e7fecf67f6d3d0a7389cf2810af692697
Greg Sochanik <greg@sochanik.com>
2016-02-19 14:58:05 +0000

    Tidy code a little and added prettyprint

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 'use strict';
-var fs = require('fs');
-var BPromise = require('bluebird');
-var request = require('request');
-request = BPromise.promisifyAll(request);
-var commandLineArgs = require('command-line-args');
-var base64 = require('base64-js');
-var options = commandLineArgs([
-  { name: 'image', alias: 'i', type: String, defaultValue: './vivaldi.jpg' },
-  { name: 'type', alias: 't', type: String, defaultValue: 'LABEL_DETECTION' },
-  { name: 'limit', alias: 'l', type: Number, defaultValue: 1 }
-]).parse();
+const fs = require('fs'),
+  BPromise = require('bluebird'),
+  commandLineArgs = require('command-line-args'),
+  base64 = require('base64-js'),
+  prettyjson = require('prettyjson');
+
+const request = BPromise.promisifyAll(require('request'));
+
+const options = commandLineArgs([
+    { name: 'image', alias: 'i', type: String, defaultValue: './vivaldi.jpg' },
+    { name: 'type', alias: 't', type: String, defaultValue: 'LABEL_DETECTION' },
+    { name: 'limit', alias: 'l', type: Number, defaultValue: 1 }
+  ]).parse();
 
 console.log(options);
 
-var image = fs.readFileSync(options.image);
-var payload = {
+const image = fs.readFileSync(options.image);
+const payload = {
   requests: [
     {
       image: {
@@ -28,7 +30,7 @@ var payload = {
   ]
 };
 
-var url = 'https://vision.googleapis.com/v1/images:annotate?key=' + process.env.GOOGLE_API_KEY;
+const url = 'https://vision.googleapis.com/v1/images:annotate?key=' + process.env.GOOGLE_API_KEY;
 
 request.postAsync({
     headers: {'content-type': 'application/json'},
@@ -36,5 +38,5 @@ request.postAsync({
     json: payload
   })
   .then(result => {
-    console.log(JSON.stringify(result.body.responses));
+    console.log(prettyjson.render(result.body.responses));
   });
